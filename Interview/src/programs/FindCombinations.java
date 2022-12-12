93dcfdf09b2e4ead1f523e6a476332ad11e0fa82
--replace-all <--replace-all>
2022-12-12 22:04:58 +0530

    interview

--- Interview/src/programs/FindCombinations.java
+++ Interview/src/programs/FindCombinations.java
@@ -1,14 +1,56 @@
 package programs;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 //not solved
 public class FindCombinations {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        int[] myArr={2,5,7,9,10,3,4, 6};
-        findCombinations(myArr,16);
+        Integer[] myArr={2,5,7,9,10,3,4, 6};
+       // findCombinations(myArr,16);
+        uniqueCombination(myArr,16);
     }
 
-    private static void findCombinations(int[] arr,int sum){
+    private static void uniqueCombination(Integer[] myArr, int k) {
+        System.out.println("-------uniqueCombination----------!");
+        List<Integer> myList= Arrays.asList(myArr);
+        List<Integer> local= new ArrayList<>();
+        Collections.sort(myList);
+        findUniqueCombination(0,0,k,local,myArr);
+    }
+
+    private static void findUniqueCombination(int l, int sum, int k, List<Integer> local, Integer[] myArr) {
+
+        if(sum==k){
+            System.out.print("{");
+            for (int i = 0; i < local.size(); i++) {
+                if (i != 0)
+                    System.out.print(" ");
+                System.out.print(local.get(i));
+                if (i != local.size() - 1)
+                    System.out.print(", ");
+            }
+            System.out.println("}");
+        }
+
+        for (int i = l; i < myArr.length; i++) {
+            if (sum + myArr[i] > k)
+                continue;
+
+            if (i > l && myArr[i] == myArr[i-1] )
+                continue;
+
+            local.add(myArr[i]);
+            findUniqueCombination(i+1,sum+myArr[i],k,local,myArr);
+            local.remove(local.size() - 1);
+        }
+    }
+
+    private static void findCombinations(Integer[] arr,int sum){
 
         for(int i=0;i<arr.length-2;i++) {
             for (int j = i + 1; j < arr.length - 1; j++) {
